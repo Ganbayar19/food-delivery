dc6b36c567516ed8780d37deed6bd19018ac82f4
Ganbayar19 <ganbayargantuya9@gmail.com>
2026-01-26 22:35:57 +0800

    fix: simplify database connection string in index.ts

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -1,11 +1,7 @@
 import { connect } from "mongoose";
 
 export const connectToDatabase = async () => {
-    await connect(
-
-      "mongodb+srv://<ganbayar19>:<GnBYR19.>@serap.sylu06t.mongodb.net/?appName=Serap"
-
-      "mongodb+srv://ganbayar19:<G.ganbayar.19>@serap.sylu06t.mongodb.net"
-
-    );
-}
+  await connect(
+    "mongodb+srv://ganbayar19:<G.ganbayar.19>@serap.sylu06t.mongodb.net",
+  );
+};
